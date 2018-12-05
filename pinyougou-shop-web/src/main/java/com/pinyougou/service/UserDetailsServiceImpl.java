ad8fd97f12594f3eeeacbf5248a096a85edabf73
LANHM\lanhongming <lanhongming@kaiheikeji.com>
2018-12-05 10:05:44 +0800

    登录验证
    密码校验

--- pinyougou-shop-web/src/main/java/com/pinyougou/service/UserDetailsServiceImpl.java
+++ pinyougou-shop-web/src/main/java/com/pinyougou/service/UserDetailsServiceImpl.java
@@ -1,14 +1,43 @@
 package com.pinyougou.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import com.pinyougou.pojo.TbSeller;
+import com.pinyougou.sellergoods.service.SellerService;
+
+/**
+ * 认证类
+ * @author lanhongming
+ *
+ */
 public class UserDetailsServiceImpl implements UserDetailsService {
+	
+	
+	public SellerService sellerService;
+	public void setSellerService(SellerService sellerService) {
+		this.sellerService = sellerService;
+	}
+
 
 	@Override
-	public UserDetails loadUserByUsername(String arg0) throws UsernameNotFoundException {
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		System.out.println("用户登录权限校验……");
+		//构建角色列表
+		List<GrantedAuthority> grantAuths = new ArrayList<>();
+		grantAuths.add(new SimpleGrantedAuthority("ROLE_SELLER"));
 		
+		TbSeller seller = sellerService.findOne(username);
+		if (seller != null && seller.getStatus().equals("1")) {
+			return new User(username, seller.getPassword(), grantAuths);
+		}
 		return null;
 	}
 
